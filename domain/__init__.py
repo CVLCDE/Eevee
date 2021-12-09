d053d8f4542029c159ed69d59ad4109c4883500d
CVLCDE <paperclip382@gmail.com>
2021-12-08 22:24:14 -0700

    feat: Improved Home Styling + Layout Change
    Styling overhaul for the Homepage. Layout split into two columns with Twitter embed for Pokebeach added.

--- domain/__init__.py
+++ domain/__init__.py
@@ -1,11 +1,10 @@
-from flask import Flask 
+from flask import Flask
 from .views import views
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'secret'
+    app.config["SECRET_KEY"] = "secret"
 
     app.register_blueprint(views, url_prefix="/")
-
-
     return app
--- domain/views.py
+++ domain/views.py
@@ -1,7 +1,23 @@
 from flask import Blueprint, render_template
 
-views = Blueprint('views', __name__)
+views = Blueprint("views", __name__)
 
-@views.route('/')
+
+@views.route("/")
 def home():
     return render_template("home.html")
+
+
+@views.route("/grailed")
+def grailed_list():
+    return render_template("grailed_list.html")
+
+
+@views.route("/sets")
+def set_list():
+    return render_template("set_list.html")
+
+
+@views.route("/sets/<string:id>")
+def set_page():
+    return render_template("set_page.html")
