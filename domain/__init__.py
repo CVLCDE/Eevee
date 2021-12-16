fbddac9e67b47b2fc4696b14fbe0c415fd3acfd2
CVLCDE <paperclip382@gmail.com>
2021-12-16 11:17:09 -0700

    feat: Modified Home page, Added configurations for MongoDB / PTCGIO, Added module for PTCGIO library, Added Overall Sets page.

--- domain/__init__.py
+++ domain/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 from .views import views
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "secret"
-
+    app.config["MONGO_URI"] = "mongodb://localhost:27017/eevee"
+    app.config["POKEMONTCG_IO_API_KEY"] = "0401cc6b-674d-4446-ad81-5294a52c8cf7"
+    mongodb_client = PyMongo(app)
+    db = mongodb_client.db
     app.register_blueprint(views, url_prefix="/")
+
     return app
--- domain/views.py
+++ domain/views.py
@@ -1,21 +1,57 @@
 from flask import Blueprint, render_template
+from pokemontcgsdk import Set
 
 views = Blueprint("views", __name__)
 
 
+def get_all_sets():
+    return Set.all()
+
+
+def get_all_set_names():
+    sets = get_all_sets()
+    set_names = [tcg_set.name for tcg_set in sets][::-1]
+    return set_names
+
+
+def get_all_set_logos():
+    sets = get_all_sets()
+    set_logos = [tcg_set.images.logo for tcg_set in sets][::-1]
+    return set_logos
+
+
+def get_all_set_totals():
+    sets = get_all_sets()
+    set_totals = [tcg_set.total for tcg_set in sets][::-1]
+    return set_totals
+
+
 @views.route("/")
 def home():
-    return render_template("home.html")
+    set_names = get_all_set_names()
+    set_logos = get_all_set_logos()
+    set_totals = get_all_set_totals()
+    return render_template(
+        "home.html", set_names=set_names, set_logos=set_logos, set_totals=set_totals
+    )
 
 
 @views.route("/grailed")
 def grailed_list():
-    return render_template("grailed_list.html")
+    return render_template("grailed.html")
 
 
 @views.route("/sets")
 def set_list():
-    return render_template("set_list.html")
+    set_names = get_all_set_names()
+    set_logos = get_all_set_logos()
+    set_totals = get_all_set_totals()
+    return render_template(
+        "sets.html",
+        set_names=set_names,
+        set_logos=set_logos,
+        set_totals=set_totals,
+    )
 
 
 @views.route("/sets/<string:id>")
